b8ce633241e269dfd22feb89547334fbb4d07ff4
Ethan Gregory Clark <ethangclark@gmail.com>
2023-01-12 07:28:42 -0700

    dependencies and implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,48 @@
 import * as React from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>Welcome to your first test package.</div>;
-};
+const resetters: Array<Function> = [];
+
+export function createAtom<State extends { [key: string]: any }>(
+  initial: State
+): {
+  get: () => State;
+  reset: () => void;
+  set: (state: State) => void;
+  use: () => State;
+  subscribe: (subFn: (state: State, previous: State) => void) => () => void;
+} {
+  let state = initial;
+
+  const reset = () => {
+    state = initial;
+  };
+  resetters.push(reset);
+
+  const get = () => state;
+
+  let subscribers = new Set<(state: State, previous: State) => void>();
+  function subscribe(subscriber: (state: State, previous: State) => void) {
+    subscribers.add(subscriber);
+    return () => {
+      subscribers.delete(subscriber);
+    };
+  }
+
+  function set(s: State) {
+    const previous = state;
+    state = s;
+    subscribers.forEach((subscriber) => subscriber(state, previous));
+  }
+
+  function use() {
+    const [s, setS] = React.useState(state);
+    React.useEffect(() => subscribe(setS), []);
+    return s;
+  }
+
+  return { get, reset, set, use, subscribe };
+}
+
+export function resetAtoms() {
+  resetters.forEach((reset) => reset());
+}
